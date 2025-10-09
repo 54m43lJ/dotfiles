e25fb5c06aa7e0a0b75ac82baea9a85bd021b906
54m43lJ <mr5ama3l97@gmail.com>
2025-10-09 15:46:43 +0800

    suspenssssse

--- src/suspend.sh
+++ src/suspend.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sudo bash -c 'echo LID0 >/proc/acpi/wakeup'
+lid_enabled() {
+  sudo bash -c 'cat /proc/acpi/wakeup | grep -Eq LID0.+\*enabled'
+}
+
+if lid_enabled; then
+  sudo bash -c 'echo LID0 >/proc/acpi/wakeup'
+fi
 systemctl suspend
