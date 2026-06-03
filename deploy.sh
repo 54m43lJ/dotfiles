6543358a642865b8058675678c1f2b90955a87d3
54m43lJ <mr5ama3l97@gmail.com>
2026-06-03 21:20:17 +0800

    fix: GRUB_SAVEDEFAULT=false, GRUB_DEFAULT=0 fixes Btrfs blocklist error; ask iGPU before setting fixed GFXMODE

--- deploy.sh
+++ deploy.sh
@@ -67,10 +67,12 @@ cp -r "$WD/electron-apps/"* ~/.config/
 # --- GRUB theme ---
 sudo cp -r "$WD/arch-linux" /boot/grub/themes/
 sudo sed -i -E 's/^(GRUB_TIMEOUT=).*$/\130/g' /etc/default/grub
-sudo sed -i -E 's/^(GRUB_DEFAULT=).*$/\1saved/g' /etc/default/grub
-sudo sed -i -E 's/^(GRUB_GFXMODE=).*$/\11280x720/g' /etc/default/grub
+sudo sed -i -E 's/^(GRUB_DEFAULT=).*$/\10/g' /etc/default/grub
+if ! confirm "Do you have integrated graphics (iGPU)?"; then
+    sudo sed -i -E 's/^(GRUB_GFXMODE=).*$/\11280x720/g' /etc/default/grub
+fi
 sudo sed -i -E 's/^#(GRUB_THEME=).*$/\1"\/boot\/grub\/themes\/arch-linux\/theme\.txt"/g' /etc/default/grub
-sudo sed -i -E 's/^#(GRUB_SAVEDEFAULT=true).*$/\1/g' /etc/default/grub
+sudo sed -i -E 's/^(GRUB_SAVEDEFAULT=).*$/\1false/g' /etc/default/grub
 sudo sed -i -E 's/^#(GRUB_DISABLE_OS_PROBER=false).*$/\1/g' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
