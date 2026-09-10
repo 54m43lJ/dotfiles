936e65ef41003c6e58bb72d2b475e534bb676193
54m54lJ <mr5ama3l97@gmail.com>
2026-09-10 14:16:00 +0800

    dev: add Rust toolchain to language module

--- dev/module.sh
+++ dev/module.sh
@@ -5,7 +5,7 @@ install_module() {
     log "Setting up developer environment..."
 
     # Language toolchains: per-language multi-select, all on by default
-    local -a langs=("Python" "C/C++" "Node.js")
+    local -a langs=("Python" "C/C++" "Rust" "Node.js")
     local -a sel=("${langs[@]}")
     local lang
     if select_multi sel "${langs[@]}"; then
@@ -13,6 +13,9 @@ install_module() {
             case "$lang" in
                 Python)    pac_ins python ;;
                 "C/C++")   pac_ins gcc make ;;
+                Rust)      pac_ins rustup
+                           rustup default stable
+                           rustup component add rust-src clippy rustfmt ;;
                 "Node.js") pac_ins npm ;;
             esac
         done
